c99de94c5ddd5844b846ed32760539ff9a580da2
Cesar Gallardo <kaesar_gallardo@hotmail.com>
2022-12-28 13:11:40 -0700

    Add project files.

---
+++ EncryptionDecryptionConsole/EncryptDecrypt.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace EncryptionDecryptionConsole
+{
+    public sealed class EncryptDecrypt
+    {
+        private static string key = "nonlja7teweflfadruna7esivazaquxu";
+        public string EncryptString(string plainText)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aesService = Aes.Create())
+            {
+                aesService.Key = Encoding.UTF8.GetBytes(key);
+                aesService.IV = iv;
+
+                ICryptoTransform encryptionService = aesService.CreateEncryptor(aesService.Key, aesService.IV);
+
+                using (MemoryStream memStream = new MemoryStream())
+                {
+                    using (CryptoStream crypStream = new CryptoStream((Stream)memStream, encryptionService, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter strWriter = new StreamWriter((Stream)crypStream))
+                        {
+                            strWriter.Write(plainText);
+                        }
+                        array = memStream.ToArray();
+                    }
+
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        public string DecryptString(string encryptedText)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(encryptedText);
+
+            using (Aes aesService = Aes.Create())
+            {
+                aesService.Key = Encoding.UTF8.GetBytes(key);
+                aesService.IV = iv;
+
+                ICryptoTransform dencryptionService = aesService.CreateDecryptor(aesService.Key, aesService.IV);
+
+                using (MemoryStream memStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream crypStream = new CryptoStream((Stream)memStream, dencryptionService, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader strReader = new StreamReader((Stream)crypStream))
+                        {
+                            return strReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ EncryptionDecryptionConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EncryptionDecryptionConsole;
+
+//Console.WriteLine("Hello, World!");
+string stringTest = string.Empty;
+stringTest = Console.ReadLine();
+
+EncryptDecrypt service = new EncryptDecrypt();
+string encryptedText = service.EncryptString(stringTest);
+string decryptedText = service.DecryptString(encryptedText);
+
+Console.WriteLine(encryptedText);
+Console.WriteLine(decryptedText);
+
+Console.ReadLine();
